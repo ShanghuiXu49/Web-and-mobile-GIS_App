f786b77ded26428dd635f2e76e689d19b4375489
ShanghuiXu49 <xushanghui49@outlook.com>
2020-04-07 07:54:57 -0400

    Fix the bug that alert the question information after user submit answer rather than the correct answer number

--- js/getCorrectAnswer.js
+++ js/getCorrectAnswer.js
@@ -1,15 +1,16 @@
 function getCorrectAnswer() {
-    var serviceUrl = "https://developer.cege.ucl.ac.uk:" + httpsPortNumberAPI + "/getCorrectAnswer/" + httpsPortNumberAPI
+    var serviceUrl = "https://developer.cege.ucl.ac.uk:" + httpsPortNumberAPI + "/getCorrectAnswer/" + httpsPortNumberAPI;
    $.ajax({
     url: serviceUrl,
     crossDomain: true,
     type: "GET",
-    success: function(data){console.log(data); processCorrectAnswer();},
-    data: postString
-}); 
-}
+    success: function(num_questions){
+    	console.log(num_questions); 
+    	processCorrectAnswer(num_questions);
+    }}); //end of the AJAX call
+}// end of getCorrectAnswer
 
-function processCorrectAnswer(){
-	var correctNumber = client.responseText;
-	alert("You have answered " + correctNumber + " questions correctly!")
-	}
+
+function processCorrectAnswer(num_questions){ 
+ alert("You have answered " + num_questions + " questions correctly!");
+ }
