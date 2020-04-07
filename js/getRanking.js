b8fab0cace595a6fdfa2769d8f75aa4df0599c7c
ShanghuiXu49 <xushanghui49@outlook.com>
2020-04-07 08:42:34 -0400

    Complete the getRanking function to alert the rank when user click get your ranking menu

--- js/getRanking.js
+++ js/getRanking.js
@@ -1,3 +1,16 @@
-function getRanking(){
-	alert("This is your ranking!");
+function getRanking() {
+    var serviceUrl = "https://developer.cege.ucl.ac.uk:" + httpsPortNumberAPI + "/getRanking/" + httpsPortNumberAPI;
+   $.ajax({
+    url: serviceUrl,
+    crossDomain: true,
+    type: "GET",
+    success: function(result){
+    	console.log(result); 
+    	processCorrectAnswer(result);
+    }}); //end of the AJAX call
+}// end of getCorrectAnswer
+
+
+function processCorrectAnswer(result){ 
+    alert("Your current ranking is No." +  result);
 }
