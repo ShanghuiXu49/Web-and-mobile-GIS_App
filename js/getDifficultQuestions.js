11f471886404ab6a58ac53742159cd47aab4f888
ShanghuiXu49 <xushanghui49@outlook.com>
2020-04-08 09:33:33 -0400

    Show the 5 most difficult questions in alert

--- js/getDifficultQuestions.js
+++ js/getDifficultQuestions.js
@@ -1,11 +1,3 @@
-var difficultQuestionLayer;
-
-function removeDifficultQuestions() {
-    alert("5 Most difficult Quiz Questions will be removed");
-    mymap.removeLayer(difficultQuestionLayer);
-};
-
-
 function getDifficultQuestions() {
     var serviceUrl = "https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI + "/getDifficultQuestions";
    $.ajax({
@@ -14,39 +6,22 @@ function getDifficultQuestions() {
     type: "GET",
     success: function(result){
         console.log(result); 
-        loaddifficultQuestion(result);
+        loadDifficultQuestion(result);
     }}); //end of the AJAX call
 }// end of getCorrectAnswer
 
 // we can also use this to determine distance for the proximity alert
 
 
-function loaddifficultQuestion(result) {
+function loadDifficultQuestion(result) {
+    var difficultQuiz = result[0].array_to_json
+    var difficultQuestionString
 
-// load the geoJSON layer
-    difficultQuestionLayer = L.geoJson(result,
-        {       
-
-// use point to layer to create the points
-            pointToLayer: function (feature, latlng)
-            {
-            // in this case, we build an HTML DIV string
-            // using the values in the data
-            var htmlString = "<DIV id='popup'"+ feature.properties.id + "><h2>" +
-            feature.properties.question_title + "</h2><br>";
-            htmlString = htmlString + "<h3>"+feature.properties.question_text +
-            "</h3><br>";
-            htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.properties.id+"_1'/>"+feature.properties.answer_1+"<br>";
-            htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.properties.id+"_2'/>"+feature.properties.answer_2+"<br>";
-            htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.properties.id+"_3'/>"+feature.properties.answer_3+"<br>";
-            htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.properties.id+"_4'/>"+feature.properties.answer_4+"<br>";
-            htmlString = htmlString + "<button onclick='checkAnswer(" + feature.properties.id + ");return false;'>Submit Answer</button>";
-            // now include a hidden element with the answer
-            // in this case the answer is alwasy the first choice
-            // for the assignment this will of course vary - you can use feature.properties.correct_answer
-            htmlString = htmlString + "<div id=answer" + feature.properties.id + " hidden>" + feature.properties.correct_answer + "</div>";
-            htmlString = htmlString + "</div>";
-            return L.marker(latlng, {icon: testMarkerOrange}). bindPopup(htmlString);
-            },
-        }).addTo(mymap);
-};
+    for(var i =0;i<difficultQuiz.length;i++)
+    {
+        difficultQuestionString +=  "Question from: " + difficultQuiz[i].port_id + "\nQuestion Text: " + difficultQuiz[i].question_text;
+        difficultQuestionString += "\nCorrect Answer: " + difficultQuiz[i]["answer_" + difficultQuiz[i].correct_answer]+"\n\n";
+    }
+    // List the 5 most difficult questions in alert
+    alert(difficultQuestionString);
+}
